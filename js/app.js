45904a4200de013c19fff981d9c9cde3d754e342
Tani Robinson <tanisha.robinson94@aol.com>
2021-03-02 19:31:27 -0500

    JS mostly complete

--- js/app.js
+++ js/app.js
@@ -18,10 +18,11 @@ function getData(event) {
        const info = searchData.map( (result) =>{
             return {
                 titles: result.volumeInfo.title,
-                authors: result.volumeInfo.authors[0],
+                authors: result.volumeInfo && result.volumeInfo.authors ? result.volumeInfo.authors[0] : "no author",
                 publishDate: result.volumeInfo.publishedDate,
-                description: result.volumeInfo.description,
-                img: result.volumeInfo.imageLinks.thumbnail
+                description: result.volumeInfo.description ?? "No description", //nullish coal js
+                img: result.volumeInfo && result.volumeInfo.imageLinks ? result.volumeInfo.imageLinks.thumbnail : "https://i.imgur.com/kG84Vg8.jpg",
+                genre: result.volumeInfo.categories
             }
        })
     //    console.log(info)
@@ -33,20 +34,22 @@ function getData(event) {
 );
 }
 
-getData();
-
 function render(info){
     console.log("Data in render function: ", info)
+    $("#results").empty();
     info.forEach( (result) =>{
         var div = $("<div></div>")
-        div.append("<h3>" + result.titles + "</h3>")
+        div.append("<h3>Title: " + result.titles + "</h3>")
         div.append("<img src=" + result.img + ">")
-        div.append("<p>" + result.authors + "</p>")
-        div.append("<p>" + result.publishDate + "</p>")
+        div.append("<p>Author: " + result.authors + "</p>")
+        div.append("<p>Date published: " + result.publishDate + "</p>")
+        div.append("<p>Genre: " + result.genre + "</p>" )
         div.append("<p>" + result.description + "</p>")
         
-        $("#main").append(div);
+        $("#results").append(div);
     }
+
     )
 }
+
 
